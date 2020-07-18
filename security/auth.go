9a5db0cff8d9cc69293b843c5d458ac2dba30c07
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2020-07-18 19:27:57 +0530

    Added IsAuthenticated field to cache the authentication details

--- security/auth.go
+++ security/auth.go
@@ -4,26 +4,32 @@ package security
 // using the database. It holds information like
 // username, password and permitted access types
 type Auth struct {
-	username string
-	password string
-	access   []Access
+	Username        string
+	Password        string
+	Access          []Access
+	IsAuthenticated bool
 }
 
 // Register function registers a new authentication detail and returns the auth object
 func Register(username string, password string, access []Access) Auth {
-	return Auth{username, password, access}
+	return Auth{username, password, access, false}
 }
 
 // Authenticate authenticates a user but does not handles authorization
 // over the database resources!
-func (auth Auth) Authenticate(username string, password string) bool {
-	return username == auth.username && password == auth.password
+func (auth *Auth) Authenticate(username string, password string) bool {
+	if username == auth.Username && password == auth.Password {
+		auth.IsAuthenticated = true
+		return true
+	}
+
+	return false
 }
 
 // Authorize method just authorizes a given action and doesn't handle
 // authentication. For authentication Authenticate method should be used
 func (auth Auth) Authorize(reqAccess Access) bool {
-	for _, access := range auth.access {
+	for _, access := range auth.Access {
 		if access == reqAccess {
 			return true
 		}
