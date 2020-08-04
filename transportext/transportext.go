41999a925790710922aa8bc98aaaddb4d3cade07
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2020-08-04 19:29:40 +0530

    Added extension for transport layer. It will be responsible for communicating events to the clients

---
+++ transportext/transportext.go
@@ -0,0 +1,41 @@
+package transportext
+
+import (
+	"github.com/utkarsh-pro/RapidoDB/eventbus"
+)
+
+// ClientConn interface describes the interface
+// for sending messages to the clients
+type ClientConn interface {
+	Msg(string)
+}
+
+// PingClient takes in a net.Conn object and variadic number
+// of events to which this will subscribe and will automatically
+// send them to the client
+func PingClient(c ClientConn, events ...string) {
+	chs := subscribeToEvents(events...)
+
+	// Process all the channels
+	for _, ch := range chs {
+		go func(ch eventbus.DataChannel) {
+			res := <-ch
+			c.Msg(res.String())
+		}(ch)
+	}
+}
+
+// subscribeToEvents subscribe to all the events passed
+// as function. This is a variadic function and hence can take in
+// any number of events.
+//
+// It returns DataChannelSlice which can be used to listen to messages
+// passed on the channel by the event bus
+func subscribeToEvents(events ...string) eventbus.DataChannelSlice {
+	var ch eventbus.DataChannelSlice
+	for _, e := range events {
+		ch = append(ch, eventbus.Instance.Subscribe(e, 1))
+	}
+
+	return ch
+}
