cb9f5f4fa903059284ba2e9e656c2ec38027b78d
Utkarsh Srivastava <srivastavautkarsh8097@gmail.com>
2020-08-05 01:55:23 +0530

    Removed repetitive code from the transport extension

--- transportext/transportext.go
+++ transportext/transportext.go
@@ -14,29 +14,9 @@ type ClientConn interface {
 // of events to which this will subscribe and will automatically
 // send them to the client
 func PingClient(c ClientConn, events ...string) {
-	chs := subscribeToEvents(events...)
+	muxcd := eventbus.ChannelMultiplexer(eventbus.Instance, 0, events...)
 
-	// Process all the channels
-	for _, ch := range chs {
-		go func(ch eventbus.DataChannel) {
-			for msg := range ch {
-				c.Msg(msg.String())
-			}
-		}(ch)
+	for msg := range muxcd {
+		c.Msg(msg.String())
 	}
-}
-
-// subscribeToEvents subscribe to all the events passed
-// as function. This is a variadic function and hence can take in
-// any number of events.
-//
-// It returns DataChannelSlice which can be used to listen to messages
-// passed on the channel by the event bus
-func subscribeToEvents(events ...string) eventbus.DataChannelSlice {
-	var ch eventbus.DataChannelSlice
-	for _, e := range events {
-		ch = append(ch, eventbus.Instance.Subscribe(e, 0))
-	}
-
-	return ch
 }
