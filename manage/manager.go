39bfa79246f78a89596a41797ce0fe21399f6113
Ashish <ashishjaitiwari15112000@gmail.com>
2021-03-07 13:22:38 +0530

    fixed typos

--- manage/manager.go
+++ manage/manager.go
@@ -30,7 +30,7 @@ type UnsecureStore interface {
 // Both of the parameters can be any store that satisfies the
 // UnsecureStore interface. The first store would be used to
 // store the data provided by the users while the second store
-// would be used internally store the user's info
+// would be used internally to store the user's info
 func New(unsecureStore UnsecureStore, userdb UnsecureStore) *SecureDB {
 	// Here a new DBClient has no username, password and has no privileges
 	// and are not associated with any events
